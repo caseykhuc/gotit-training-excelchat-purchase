b4dc84bc3037071b930c7b7b2f94bc7529048603
Casey Khuc <khuctrang1812@gmail.com>
2020-01-10 11:27:46 +0700

    feat: Complete multi-env config for dev/staging

--- utility/api.py
+++ utility/api.py
@@ -1,37 +1,47 @@
 import requests
 import json
-from config.main_config import AdminAccount
 
 
 class ApiRequest:
     _default_header = {
-        "Authorization": "Bearer {}".format(AdminAccount.ACCESS_TOKEN.value),
+        "Authorization": "Bearer {}",
         "x-gotit-vertical": "Excel",
     }
     _default_params = {"product": "excelchat"}
     _default_payload = {}
 
     @staticmethod
-    def with_content_type():
-        header = ApiRequest._default_header
-        header["Content-Type"] = "application/json"
-        return header
+    def with_content_type(_headers):
+        headers = _headers
+        headers["Content-Type"] = "application/json"
+        return headers
 
     @staticmethod
-    def put(url, params=_default_params, payload=_default_payload):
-        return requests.put(
-            url, params=params, json=payload, headers=ApiRequest.with_content_type(),
+    def with_admin_account(_headers, admin_account):
+        headers = _headers
+        headers["Authorization"] = _headers["Authorization"].format(
+            admin_account.ACCESS_TOKEN.value
         )
+        return headers
 
     @staticmethod
-    def post(url, params=_default_params, payload=_default_payload):
-        return requests.post(
-            url, params=params, json=payload, headers=ApiRequest.with_content_type(),
+    def put(url, admin_account, params=_default_params, payload=_default_payload):
+        headers = ApiRequest.with_content_type(ApiRequest._default_header)
+        headers = ApiRequest.with_admin_account(headers, admin_account)
+        return requests.put(url, params=params, json=payload, headers=headers,)
+
+    @staticmethod
+    def post(url, admin_account, params=_default_params, payload=_default_payload):
+        headers = ApiRequest.with_content_type(ApiRequest._default_header)
+        headers = ApiRequest.with_admin_account(headers, admin_account)
+        return requests.post(url, params=params, json=payload, headers=headers,)
+
+    @staticmethod
+    def get(url, admin_account, params=_default_params):
+        headers = ApiRequest.with_admin_account(
+            ApiRequest._default_header, admin_account
         )
-
-    @staticmethod
-    def get(url, params=_default_params):
-        return requests.get(url, params=params, headers=ApiRequest._default_header)
+        return requests.get(url, params=params, headers=headers)
 
 
 class ApiResponse:
--- utility/clean_up.py
+++ utility/clean_up.py
@@ -1,17 +1,17 @@
-from config.main_config import Url
 from utility.api import ApiRequest, ApiResponse
 
 
 class CleanUp:
-    def terminate_subscription(self):
-        subscription_id = self.get_subscription_id()
+    def terminate_subscription(self, Url, AdminAccount):
+        subscription_id = self.get_subscription_id(Url, AdminAccount)
         ApiRequest.put(
             Url.TERMINATE.value.format(subscription_id),
+            AdminAccount,
             payload={"status": "terminated", "product": "excelchat"},
         )
 
-    def get_subscription_id(self):
-        response = ApiRequest.get(Url.GET_SUBSCRIPTION.value)
+    def get_subscription_id(self, Url, AdminAccount):
+        response = ApiRequest.get(Url.GET_SUBSCRIPTION.value, AdminAccount)
         return ApiResponse.get_loaded_response(response.text)["data"][
             "latest_subscription"
         ]["id"]
