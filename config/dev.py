b592dc81d62f2b47c7b154df4416c6837751abad
Casey Khuc <khuctrang1812@gmail.com>
2020-01-13 14:33:16 +0700

    refactor: Hide private config info

--- config/dev.py
+++ config/dev.py
@@ -1,18 +1,2 @@
 from config.main_config import *
-
-
-class Url:
-    ADMIN = "http://admin.got-it.io/"
-    GET_SUBSCRIPTION = "https://api.got-it.io/admin/askers/16413/subscriptions"
-    TERMINATE = "https://api.got-it.io/admin/askers/16413/subscriptions/{}"
-    ASKER_URL = "http://www.got-it.io/solutions/excel-chat"
-
-
-class AskerAccount:
-    EMAIL = "trang+02@gotitapp.co"
-    PASSWORD = "1234aA"
-    DEFAULT_CARD = "1881"
-
-
-class AdminAccount:
-    ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImJjMzMwZjkxIiwiaWF0IjoxNTc4NDU1MTA1LCJzdWIiOjEwMCwiZXhwIjoxNjA5OTkxMTA1LCJhdWQiOiJhZG1pbiJ9.U-mgnyM4dCpK0ga4J1lWstXdQkygnw8Hxp_lmVJhgFk"
+from _private_config.dev import *
